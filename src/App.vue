3bab83d2d2f44bd70994e4408700497805415b91
thomasbridgeglobal <thomas.p@bridge-global.com>
2022-05-25 18:13:41 +0530

    init2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h2>{{ title }}</h2>
+
+  <div class="input-group">
+    <input v-model="newItem" v-on:keyup.enter="addItem" placeholder="add shopping list item" type="text" class="form-control">
+    <span class="input-group-btn">
+    <button v-on:click="addItem" class="btn btn-default" type="button">Add!</button>
+    </span>
+  </div>
+
+    <ul class="list-group"> 
+      <li class="list-group-item" v-for="item in items" v-bind:class="{ 'removed': item.checked }" :key="item.text" >
+        <div class="checkbox">
+        <label>
+        <input type="checkbox" v-model="item.checked"> {{ item.text }}
+        </label>
+        </div>
+      </li>      
+    </ul>
+    <div class="footer">
+    <hr/>
+    <em>Change the title of your shopping list here</em>
+    <input v-model="title"/>
+    </div>
+    <hello-world  msg="hello there"></hello-world>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function(){
+    return {      
+      title: "Shopping list",
+      items: [{text: "Toy guns", checked: false }, {text: "criket bat", checked: false }],
+      newItem: ""
+    }
+  },
+  methods: {
+  addItem: function () {
+    var text;
+    text = this.newItem.trim();
+    if (text) {
+      this.items.push({
+        text: text,
+        checked: false
+      });
+      this.newItem = '';
+    }
   }
+}
 }
 </script>
 
@@ -22,7 +65,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50;  
 }
+
+
+  .removed label {
+    text-decoration: line-through;
+  }
+</style>
+
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap'; 
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+require('bootstrap');
+
 Vue.config.productionTip = false
 
 new Vue({
